0dabee7b1232d187a8d7c376435513a50c0805be
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2021-08-01 23:35:31 -0700

    create history, css, link to flask

--- Server.py
+++ Server.py
@@ -3,13 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.secret_key = "something"  # figure this out l8r
 
-@app.route("/", methods=["GET", "POST"])
 
+@app.route("/", methods=["GET", "POST"])
 def home():
     """
     :return: The web page to be rendered.
     """
     return render_template("home.html")
+
+
+@app.route("/history/")
+def history():
+    return render_template("history.html")
+
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
